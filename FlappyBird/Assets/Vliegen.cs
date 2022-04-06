b0a55990a04b1e5f3176dc10b91cb84f5e6d50cd
ArwinW <239881@edu.rocfriesepoort.nl>
2022-04-06 12:06:56 +0200

    spawnerweg

--- FlappyBird/Assets/Vliegen.cs
+++ FlappyBird/Assets/Vliegen.cs
@@ -8,10 +8,12 @@ public class Vliegen : MonoBehaviour
     public float velocity = 1;
     //haalt de rigidbody op uit unity, deze voegt dingen zoals zwaarte kracht toe aan Flappy
     private Rigidbody2D rb;
+    public float levens = 3;
+    public GameObject pijp;
 
     // Start is called before the first frame update
     void Start()
-    {
+    {     
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -23,5 +25,16 @@ public class Vliegen : MonoBehaviour
             //Zet de opwaartse kracht omhoog met de velocity die is aangegeven
             rb.velocity = Vector2.up * velocity;
         }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        doodgaan();
+    }
+
+    void doodgaan()
+    {
+        levens -= 1;
+        transform.position = new Vector2(-8, 1);
+        Object.Destroy(pijp);
     }
 }
